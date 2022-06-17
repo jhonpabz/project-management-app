8fa5ed26a19eab371776360a5ae8907d9ef96f6e
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-17 15:06:03 +0800

    add mongoose models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const { projects, clients } = require("../sampleData.js");
 
+// Mongoose models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -30,7 +34,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id == parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -42,27 +46,27 @@ const RootQuery = new GraphQLObjectType({
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((project) => project.id === args.id);
+        return Client.find(args.id);
       },
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
   },
