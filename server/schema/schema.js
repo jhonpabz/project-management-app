512ebbdabe11c06937824bbc79e1f33e03b285f3
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-17 10:27:27 +0800

    add project type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,6 +19,17 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+//Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -32,7 +43,20 @@ const RootQuery = new GraphQLObjectType({
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return clients.find((project) => project.id === args.id);
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
       },
     },
   },
