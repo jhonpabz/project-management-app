b81c3dbd192a38774ae6e4e26630234fd572657f
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-19 10:52:29 +0800

    add delete client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -77,6 +77,7 @@ const RootQuery = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
+    // Add a client
     addClient: {
       type: ClientType,
       args: {
@@ -91,6 +92,16 @@ const mutation = new GraphQLObjectType({
           phone: args.phone,
         });
         return client.save();
+      },
+    },
+    // Delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndRemove(args.id);
       },
     },
   },
