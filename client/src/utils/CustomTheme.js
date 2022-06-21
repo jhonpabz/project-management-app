2228d386cc3fb6c787b35c935d25ea7a42d114c5
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-21 23:51:36 +0800

    add custom theme

---
+++ client/src/utils/CustomTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const CustomTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#111827",
+    },
+    secondary: {
+      main: "#02b0fe",
+    },
+  },
+});
