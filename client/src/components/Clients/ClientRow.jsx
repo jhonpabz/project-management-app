2a9b889b239e32351297562d4aedd3fdda895a76
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-23 16:23:46 +0800

    reload fetch data on delete

--- client/src/components/Clients/ClientRow.jsx
+++ client/src/components/Clients/ClientRow.jsx
@@ -5,10 +5,21 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../../mutations/clientMutations";
+import { GET_CLIENTS } from "../../services/clientQueries";
 
 const ClientRowComponent = ({ client }) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
   });
 
   return (
