f822cbb5448f6818e5ed098a90040780bc8bc8ff
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-24 15:53:33 +0800

    fix error

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -6,12 +6,23 @@ import ProjectCard from "../ProjectCard";
 
 const Projects = () => {
   const { loading, error, data } = useQuery(GET_PROJECTS);
-
+  console.log("projects data:", data);
   if (loading) return <LoadingSpinner />;
   if (error) return <h1>Something Went Wrong</h1>;
   return (
     <>
-      <h1>Prjects Here</h1>
+      {data.projects.length > 0 ? (
+        <div>
+          {data.projects.map((project) => (
+            // <ProjectCard key={project.id} project={project} />
+            <div key={project.id}>
+              <p>{project.name}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No Projects</p>
+      )}
     </>
   );
 };
