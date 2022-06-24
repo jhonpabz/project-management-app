769ca6f20bc4a06d653cea7e865878350ed23e1a
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-25 00:48:42 +0800

    update project cards layout

--- client/src/components/ProjectCard/index.jsx
+++ client/src/components/ProjectCard/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 const ProjectCard = ({ project }) => {
-  return <div>ProjectCard</div>;
+  return (
+    <Box sx={{ m: 1 }}>
+      <Grid item xs={4}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {project.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Box>
+  );
 };
 
 export default ProjectCard;
--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -3,22 +3,29 @@ import LoadingSpinner from "../LoadingSpinner";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECTS } from "../../services/projectQueries";
 import ProjectCard from "../ProjectCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 const Projects = () => {
   const { loading, error, data } = useQuery(GET_PROJECTS);
-  console.log("projects data:", data);
+
   if (loading) return <LoadingSpinner />;
   if (error) return <h1>Something Went Wrong</h1>;
   return (
     <>
       {data.projects.length > 0 ? (
         <div>
-          {data.projects.map((project) => (
-            // <ProjectCard key={project.id} project={project} />
-            <div key={project.id}>
-              <p>{project.name}</p>
-            </div>
-          ))}
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={{ xs: 2, md: 3 }}
+              columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+              {data.projects.map((project) => (
+                <ProjectCard key={project.id} project={project} />
+              ))}
+            </Grid>
+          </Box>
         </div>
       ) : (
         <p>No Projects</p>
