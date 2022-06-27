aa0295339d8e6bb688d8d893c35af2c9b6d95018
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-06-27 11:42:46 +0800

    add new project feature

---
+++ client/src/components/AddProjectModal/index.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import { ADD_PROJECT } from "../../mutations/projectMutations";
+import { toast } from "react-toastify";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { GET_CLIENTS } from "../../services/clientQueries";
+import { GET_PROJECTS } from "../../services/projectQueries.js";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const AddProjectModal = () => {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, status, clientId },
+    update(cache, { data: { addClient } }) {
+      const { projects } = cache.readQuery({
+        query: GET_PROJECTS,
+      });
+
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "" || description === "" || status === "") {
+      return alert("Please fill in all  fields");
+    }
+
+    addProject(name, description, status, clientId);
+
+    setName("");
+    setDescription("");
+    setStatus("new");
+    setClientId("");
+    setOpen(false);
+    toast.success("Added Project Successfully");
+  };
+
+  if (loading) return null;
+  if (error) return <p>Something went wrong.</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <>
+          <Box sx={{ m: 1, mt: 2 }}>
+            <Button onClick={handleOpen} variant="outlined">
+              New Project
+            </Button>
+          </Box>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                New Project
+              </Typography>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  id="standard-basic"
+                  label="Name"
+                  variant="standard"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  fullWidth
+                />
+                <Box sx={{ mt: 1 }}>
+                  <TextareaAutosize
+                    aria-label="minimum height"
+                    minRows={3}
+                    placeholder="Description"
+                    style={{ width: "100%" }}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </Box>
+
+                <FormControl
+                  fullWidth
+                  variant="standard"
+                  sx={{ m: 1, minWidth: 120 }}
+                >
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Status
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                    label="Status"
+                  >
+                    <MenuItem value="new">Not Started</MenuItem>
+                    <MenuItem value="progress">In Progress</MenuItem>
+                    <MenuItem value="completed">Completed</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <FormControl
+                  fullWidth
+                  variant="standard"
+                  sx={{ m: 1, minWidth: 120 }}
+                >
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Clients
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={clientId}
+                    onChange={(e) => setClientId(e.target.value)}
+                    label="Select Client"
+                  >
+                    {data.clients.map((client) => (
+                      <MenuItem key={client.id} value={client.id}>
+                        {client.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+
+                <Box sx={{ mt: 2 }}>
+                  <Button type="Submit" variant="contained" fullWidth>
+                    Submit
+                  </Button>
+                </Box>
+              </form>
+            </Box>
+          </Modal>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AddProjectModal;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AddModalClient from "../../components/AddModalClient";
+import AddProjectModal from "../../components/AddProjectModal";
 import Clients from "../../components/Clients";
 import Projects from "../../components/Projects";
 const Home = () => {
   return (
     <>
       <AddModalClient />
+      <AddProjectModal />
       <Projects />
       <Clients />
     </>
